032a0eea54fbc4484d6446beebb8912bbadfdfee
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-12-24 01:14:09 +0100

    v0.0.4

--- test/aux.js
+++ test/aux.js
@@ -5,3 +5,8 @@ var aux = new Aux({silent: true});
 aux.local('echo "TEST"', 'ls');
 aux.local('ls /').stdout.pipe(process.stdout);
 
+console.log('remote');
+var test = aux.Remote({host: 'test.nosco'});
+test('ls', 'cat aux_test');
+test('echo "TESTING" > aux_test2');
+
