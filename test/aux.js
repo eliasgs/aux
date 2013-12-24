f9ef043ba7422f1a668bd675904baeaabb323245
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-12-24 10:37:43 +0100

    Add some simple preliminary tests

--- test/aux.js
+++ test/aux.js
@@ -2,11 +2,15 @@ var assert = require('assert');
 var Aux = require('../lib/aux');
 var aux = new Aux({silent: true});
 
+// Preliminary tests
 aux.local('echo "TEST"', 'ls');
 aux.local('ls /').stdout.pipe(process.stdout);
 
 console.log('remote');
-var test = aux.Remote({host: 'test.nosco'});
+var test = aux.Remote({host: 'test.aux-remote'});
 test('ls', 'cat aux_test');
 test('echo "TESTING" > aux_test2');
 
+if (process.argv[2] == 'remove') {
+  test('rm aux_test2');
+}
